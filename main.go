f2df0b6ac0d0d252ac81da6613a6b3cd117ea976
Pavel Zaitsev <pavel@arslogi.ca>
2019-11-18 00:05:20 +0300

    serves images by database id number now

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pavelz/insta-go/ent"
-	"github.com/pavelz/insta-go/ent/user"
+	"github.com/pavelz/insta-go/ent/photo"
 	"github.com/pavelz/insta-go/src/location"
+	"encoding/json"
 	"log"
 	"os"
+	"strconv"
+	"unsafe"
 )
 import "fmt"
 import "net/http"
@@ -25,7 +28,10 @@ func Exists(name string) bool {
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
+	println("HANDLOR")
 	f := r.URL.Path[1:]
+
+	println(f)
 	if(r.Method == "POST"){
 		// read photo and store it
 		fmt.Printf("a POST request!\n")
@@ -48,11 +54,11 @@ func handler(w http.ResponseWriter, r *http.Request){
 		fmt.Printf("file: %s saved of %d bytes\n", header.Filename, header.Size)
 
 		client := doConnection()
-		_, err = client.User.
+		_, err = client.Photo.
 			Create().
-			SetAge(30).
-			SetName("whot").
-			SetNick("gimmiphotos").
+			SetLat(11.11).
+			SetLng(12.12).
+			SetFielname("gimmiphotos.jpg").
 			SetImage(data).
 			Save(context.Background())
 
@@ -61,10 +67,10 @@ func handler(w http.ResponseWriter, r *http.Request){
 			os.Exit(-1)
 		}
 
-
 	} else {
 		if (Exists(f)) {
 			// serve the file
+			println("EXXISZ")
 			file, err := os.OpenFile(f, 0, os.FileMode(0777))
 
 			if (err != nil) {
@@ -78,14 +84,15 @@ func handler(w http.ResponseWriter, r *http.Request){
 		} else {
 			// check if there is a record with this name:
 			client := doConnection()
-			res := client.User.Query().Where(user.Name(f))
+			res := client.Photo.Query().Where(photo.Fielname(f))
 			c, err := res.Count(context.Background())
 			if err != nil {
 				fmt.Printf("Error: %s\n", err)
 				os.Exit(-1)
 			}
+			fmt.Printf("Count %d\n", c)
 
-				if c > 0 {
+			if c > 0 {
 				d, err := res.First(context.Background())
 				if err != nil {
 					fmt.Printf("error!: %s\n", err)
@@ -96,7 +103,6 @@ func handler(w http.ResponseWriter, r *http.Request){
 		}
 	}
 }
-
 
 // Ent example
 func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
@@ -153,9 +159,104 @@ func doLocation(w http.ResponseWriter, r *http.Request){
 
 func main() {
 	fmt.Println("insta-go 0.01a")
-
+	http.HandleFunc("/alive", alive)
+	http.HandleFunc("/users/sign_in", usersJASON)
+	http.HandleFunc("/photos/", photoJASON)
+	http.HandleFunc("/photos", newPhotosJASON)
+	http.HandleFunc("/photos.json", photosJASON)
 	http.HandleFunc("/ent", entTest)
 	http.HandleFunc("/locations", doLocation)
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3001", nil))
 }
+
+func photoJASON(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+
+		f := request.URL.Path
+		var id int = 0
+		n, err := fmt.Sscanf(f,"/photos/%d",&id)
+		println(err)
+		println(n)
+		fmt.Printf("Files %s , %d", f, id)
+		client := doConnection()
+		res := client.Photo.Query().Where(photo.ID(id))
+		c, err := res.Count(context.Background())
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			os.Exit(-1)
+		}
+		fmt.Printf("Count %d\n", c)
+
+		if c > 0 {
+			d, err := res.First(context.Background())
+			if err != nil {
+				fmt.Printf("error!: %s\n", err)
+			}
+
+			writer.Write(d.Image)
+		}
+		return
+	}
+}
+
+func alive(writer http.ResponseWriter, request *http.Request) {
+	fmt.Printf("CHECK ALIVE JASON ✌️\n")
+	writer.Write([]byte("✌️"))
+}
+
+
+func newPhotosJASON(writer http.ResponseWriter, request *http.Request) {
+	println("NEW PHOTOS JASON")
+
+	a_file, header, _ := request.FormFile("photo[image]")
+	println("JASON does photos")
+	data := make([]byte, header.Size)
+	_, err := a_file.Read(data)
+	if err != nil {
+		fmt.Printf("error %s\n", err)
+	}
+	var f float64
+	lat, err := strconv.ParseFloat(request.FormValue("location[lat]"),int(unsafe.Sizeof(f)))
+	if err != nil {
+		lat = 0.0
+	}
+
+	lng, err := strconv.ParseFloat(request.FormValue("location[lat]"),int(unsafe.Sizeof(f)))
+	if err != nil {
+		lng = 0.0
+	}
+
+	client := doConnection()
+	_, err = client.Photo.
+		Create().
+		SetLat(lat).
+		SetLng(lng).
+		SetFielname(request.FormValue("photo[name]")).
+		SetImage(data).
+		Save(context.Background())
+
+	_, _ = writer.Write([]byte("OK👍"))
+}
+
+func usersJASON(writer http.ResponseWriter, request *http.Request) {
+	fmt.Printf("JASON USERS\n")
+	_, _ = writer.Write([]byte("{\"authentication_token\": \"abc\", \"email\": \"pavel@spam.ca\"}"))
+}
+
+func photosJASON(writer http.ResponseWriter, request *http.Request) {
+	println("JASON WAS CALLED")
+	client := doConnection()
+	data, err := client.Photo.Query().All(context.Background())
+	if err != nil {
+		fmt.Printf("Error fetch list of photos: %s\n", err)
+		return
+	}
+	jason, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("Error formatting JASON output: %s\n", err)
+	}
+	writer.Header().Set("AContent-Type","application/json")
+	writer.Write(jason)
+}
+
